d69300250b4ff14edb7321e54d2e78b5f47356b2
system-auto-jk <systemautojk@gmail.com>
2025-01-21 10:35:45 -0300

    Update Requests_Gran_TRIBUNAIS.py

--- Requests_Gran_TRIBUNAIS.py
+++ Requests_Gran_TRIBUNAIS.py
@@ -2,16 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 from time import sleep
 
-def Infor_GRAN_TRIBUNAIS():
+carreira = ('policia','concurso-fiscal','tribunais','concurso-juridico','concurso-professor','concurso-militar','area-saude','area-ti','diplomata')
 
-    URL_GRAN_TRIBUNAIS = requests.get('https://blog.grancursosonline.com.br/editorias/tribunais/')
+
+def Infor_GRAN_TRIBUNAIS(carreira):
+
+    URL_GRAN_TRIBUNAIS = requests.get(f'https://blog.grancursosonline.com.br/editorias/{carreira}/')
 
     conteudo_GRAN_Tribunais = BeautifulSoup(URL_GRAN_TRIBUNAIS.content, 'html.parser')
 
     box_titulos_GRAN_Tribunais = conteudo_GRAN_Tribunais.find_all("div", attrs={'class':'row list-post'})
 
     for titulo in box_titulos_GRAN_Tribunais:
-        print('CONCURSOS - TRI / JUR (GRAN)',f'{titulo.find("img")["alt"].strip()}\n{titulo.find("a")["href"]}\n-')
+        print(f'{titulo.find("img")["alt"].strip()}\n{titulo.find("a")["href"]}\n-')
         sleep(1)
 
-Infor_GRAN_TRIBUNAIS()
+#Infor_GRAN_TRIBUNAIS(carreira[0])
+#ou
+Infor_GRAN_TRIBUNAIS('policia')
